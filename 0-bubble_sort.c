c7009f9163f59a7e4462f2fc9d56726929829b1e
RewanAbdulkariem <rewan.khaled21@gmail.com>
2024-07-27 03:28:07 +0300

    add documentation

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,10 @@
 #include "sort.h"
 
+/**
+ * bubble_sort - the simplest sorting algorithm
+ * @array: array to be sorted
+ * @size: size of array
+ */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
