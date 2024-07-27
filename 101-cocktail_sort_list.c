94a24786215b0d51b839b7e4afdf4efd58c8c043
RewanAbdulkariem <rewan.khaled21@gmail.com>
2024-07-27 22:48:08 +0300

    advanced

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -5,45 +5,45 @@
  */
 void cocktail_sort_list(listint_t **list)
 {
-    listint_t *start, *end;
-    int swapped;
+	listint_t *start, *end;
+	int swapped;
 
-    if (!list || !(*list) || !(*list)->next)
-        return;
+	if (!list || !(*list) || !(*list)->next)
+		return;
 
-    end = NULL;
-    start = *list;
-    swapped = 1;
+	end = NULL;
+	start = *list;
+	swapped = 1;
 
-    while (swapped)
-    {
-        swapped = 0;
-        while (start->next && start->next != end)
-        {
-            if (start->n > start->next->n)
-            {
-                swap_nodes(list, start, start->next);
-                swapped = 1;
-            }
-            else
-                start = start->next;
-        }
-        if (!swapped)
-            break;
-        end = start;
-        swapped = 0;
-        while (end->prev && end->prev != start)
-        {
-            if (end->n < end->prev->n)
-            {
-                swap_nodes(list, end->prev, end);
-                swapped = 1;
-            }
-            else
-                end = end->prev;
-        }
-        start = *list;
-    }
+	while (swapped)
+	{
+		swapped = 0;
+		while (start->next && start->next != end)
+		{
+			if (start->n > start->next->n)
+			{
+				swap_nodes(list, start, start->next);
+				swapped = 1;
+			}
+			else
+				start = start->next;
+		}
+		if (!swapped)
+			break;
+		end = start;
+		swapped = 0;
+		while (end->prev && end->prev != start)
+		{
+			if (end->n < end->prev->n)
+			{
+				swap_nodes(list, end->prev, end);
+				swapped = 1;
+			}
+			else
+				end = end->prev;
+		}
+		start = *list;
+	}
 }
 
 /**
@@ -54,27 +54,27 @@ void cocktail_sort_list(listint_t **list)
  */
 void swap_nodes(listint_t **list, listint_t *left, listint_t *right)
 {
-    if (!left || !right || left == right)
-        return;
+	if (!left || !right || left == right)
+		return;
 
-    if (left->prev)
-        left->prev->next = right;
-    else
-        *list = right;
+	if (left->prev)
+		left->prev->next = right;
+	else
+		*list = right;
 
-    if (right->next)
-        right->next->prev = left;
+	if (right->next)
+		right->next->prev = left;
 
-    left->next = right->next;
-    right->prev = left->prev;
-    left->prev = right;
-    right->next = left;
+	left->next = right->next;
+	right->prev = left->prev;
+	left->prev = right;
+	right->next = left;
 
-    if (left->next)
-        left->next->prev = left;
+	if (left->next)
+		left->next->prev = left;
 
-    if (right->prev)
-        right->prev->next = right;
+	if (right->prev)
+		right->prev->next = right;
 
-    print_list(*list);
+	print_list(*list);
 }
